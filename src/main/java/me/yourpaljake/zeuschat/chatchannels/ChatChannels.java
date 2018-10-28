b5c96fc2d7febfd69628b857777a1264eb0bd1a8
YourPalJake <jtmnijssen@outlook.com>
2018-10-28 16:23:24 +0100

    Patch Notes: - Added toggledHistory to get the previous channel the player was   toggled to. This is for a placeholder

--- src/main/java/me/yourpaljake/zeuschat/chatchannels/ChatChannels.java
+++ src/main/java/me/yourpaljake/zeuschat/chatchannels/ChatChannels.java
@@ -11,11 +11,13 @@ import org.bukkit.plugin.Plugin;
 import java.util.HashMap;
 import java.util.UUID;
 
+@SuppressWarnings("all")
 public class ChatChannels implements IModule {
 
     private HashMap<String, ChatChannel> chatChannels = new HashMap<>();
     private HashMap<String, ChatChannel> prefixes = new HashMap<>();
     private HashMap<UUID, ChatChannel> toggled = new HashMap<>();
+    private HashMap<UUID, ChatChannel> toggledHistory = new HashMap<>();
 
     private ConfigurationSection configSect;
 
@@ -118,8 +120,24 @@ public class ChatChannels implements IModule {
      * @return toggled
      */
     public boolean isToggled(Player player){
-        return toggled.containsKey(player.getUniqueId());
+        return isToggled(player.getUniqueId());
     }
+
+    /**
+     * Check if the player has a toggled history
+     *
+     * @param uuid The player's uuid
+     * @return toggledHistory
+     */
+    public boolean hasToggledHistory(UUID uuid) { return toggledHistory.containsKey(uuid); }
+
+    /**
+     * Check if the player has a toggled history
+     *
+     * @param player The player instance
+     * @return toggledHistory
+     */
+    public boolean hasToggledHistory(Player player) { return hasToggledHistory(player.getUniqueId()); }
 
     /**
      * Gets the ChatChannel the player is toggled to
@@ -139,8 +157,28 @@ public class ChatChannels implements IModule {
      * @return ChatChannel instance, null if not toggled to any ChatChannel
      */
     public ChatChannel getToggled(Player player){
-        if(toggled.containsKey(player.getUniqueId())) return toggled.get(player.getUniqueId());
+        return getToggled(player.getUniqueId());
+    }
+
+    /**
+     * Gets the previous ChatChannel the player was toggled to
+     *
+     * @param uuid The player's uuid
+     * @return The previous ChatChannel instance, null if no previous ChatChannel
+     */
+    public ChatChannel getToggledHistory(UUID uuid){
+        if(toggled.containsKey(uuid)) return toggledHistory.get(uuid);
         return null;
+    }
+
+    /**
+     * Gets the previous ChatChannel the player was toggled to
+     *
+     * @param player The player's instance
+     * @return The previous ChatChannel instance, null if no previous ChatChannel
+     */
+    public ChatChannel getToggledHistory(Player player){
+        return getToggledHistory(player.getUniqueId());
     }
 
     /**
@@ -154,15 +192,19 @@ public class ChatChannels implements IModule {
         if(chatChannel != null){
             if(!Bukkit.getServer().getPlayer(uuid).hasPermission(chatChannel.getWritePermission())) return false;
             if(toggled.containsKey(uuid) && forceToggle){
+                if(toggledHistory.containsKey(uuid)) toggledHistory.remove(uuid);
+                toggledHistory.put(uuid, toggled.get(uuid));
                 toggled.remove(uuid);
                 toggled.put(uuid, chatChannel);
                 //TODO Send switch between channel message
                 return true;
             }
+            if(toggledHistory.containsKey(uuid)) toggledHistory.remove(uuid);
             toggled.put(uuid, chatChannel);
             //TODO Send toggle on message
         }else{
             if(toggled.containsKey(uuid)){
+                toggledHistory.put(uuid, toggled.get(uuid));
                 toggled.remove(uuid);
                 //TODO Send toggle off message
                 return true;
@@ -179,25 +221,7 @@ public class ChatChannels implements IModule {
      * @return true successful, false had no permission to the ChatChannel or was already toggled to that ChatChannel
      */
     public boolean setToggled(Player player, ChatChannel chatChannel){
-        if(chatChannel != null){
-            if(!player.hasPermission(chatChannel.getWritePermission())) return false;
-            if(toggled.containsKey(player.getUniqueId()) && forceToggle){
-                toggled.remove(player.getUniqueId());
-                toggled.put(player.getUniqueId(), chatChannel);
-                //TODO Send switch between channel message
-                return true;
-            }
-            toggled.put(player.getUniqueId(), chatChannel);
-            //TODO Send toggle on message
-            return true;
-        }else{
-            if(toggled.containsKey(player.getUniqueId())){
-                toggled.remove(player.getUniqueId());
-                //TODO Send toggle off message
-                return true;
-            }
-        }
-        return false;
+        return setToggled(player.getUniqueId(), chatChannel);
     }
 
 
